ee873307befab48e761d8f68bfbf6a752d73c74d
Sam Kalum <stkalum@gmail.com>
2018-01-25 15:22:23 -0500

    Scraper.scrape_issues scraping issues correctly from wookieepedia

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,23 @@ require 'nokogiri'
 class Scraper
 
   def self.scrape_series(path)
-    titles = Nokogiri::HTML(open(path)).css("tr td b font[size=\"3\"]")
+    titles = Nokogiri::HTML(open(path)).css("div.CategoryTreeSection a")
 
     titles.each do |title|
-      puts title.text.strip
+      Series.find_or_create_by_name(title.text.strip).url = title["href"]
     end
+  end
+
+  def self.scrape_issues(path)
+    issues = Nokogiri::HTML(open(path)).css("div.mw-content-ltr ul li a")
+
+    issues.each do |issue|
+      if issue.text.match?(/Star\sWars\s\d+.*/)
+        puts issue.text
+        # Issues.find_or_create_by_name(issue.text.strip.url) = issue["href"]
+      end
+    end
+
   end
 
 end
